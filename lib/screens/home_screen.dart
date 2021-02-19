cfe524e9b6da989a5c7f766f78c49c9ab76bcdc6
marvin <marvinrenelopez@gmail.com>
2021-02-19 11:54:38 -0600

    Fade App Bar, Content Header, Vertical Icon Button

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_netflix_responsive_ui/data/data.dart';
 import 'package:flutter_netflix_responsive_ui/widgets/widgets.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -7,10 +8,31 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  double _scrollOffset = 0.0;
+  ScrollController _scrollController;
+
+  @override
+  void initState() {
+    _scrollController = ScrollController()
+      ..addListener(() {
+        setState(() {
+          _scrollOffset = _scrollOffset;
+        });
+      });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     final Size screenSize = MediaQuery.of(context).size;
     return Scaffold(
+      extendBodyBehindAppBar: true,
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.grey[850],
         child: const Icon(Icons.cast),
@@ -18,7 +40,17 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       appBar: PreferredSize(
         preferredSize: Size(screenSize.width, 50.0),
-        child: CustomAppBar(),
+        child: CustomAppBar(scrollOffset: _scrollOffset),
+      ),
+      body: CustomScrollView(
+        controller: _scrollController,
+        slivers: [
+          SliverToBoxAdapter(
+            child: ContentHeader(
+              featuredContent: sintelContent,
+            ),
+          )
+        ],
       ),
     );
   }
--- lib/widgets/custom_app_bar.dart
+++ lib/widgets/custom_app_bar.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_netflix_responsive_ui/assets.dart';
 
 class CustomAppBar extends StatelessWidget {
+  final double scrollOffset;
+
+  const CustomAppBar({Key key, this.scrollOffset = 0.0}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -9,7 +12,8 @@ class CustomAppBar extends StatelessWidget {
         vertical: 10.0,
         horizontal: 24.0,
       ),
-      color: Colors.orange,
+      color:
+          Colors.black.withOpacity((scrollOffset / 350).clamp(0, 1).toDouble()),
       child: SafeArea(
         child: Row(
           children: [
